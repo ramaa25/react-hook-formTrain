49d8e8469e585712cdf34b2a46c2e90482945d03
Ahmad Ramadhani <ahmadrama251005@gmail.com>
2024-05-12 21:24:35 +0700

    todo

--- app/component/Todo.tsx
+++ app/component/Todo.tsx
@@ -1,19 +1,14 @@
-import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
-import Task from "./Task";
-import {
-  AiOutlineCheckCircle,
-  AiOutlineDelete,
-  AiOutlineEdit,
-  AiOutlineFileDone,
-} from "react-icons/ai";
-import Sidebar from "./Sidebar";
+import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import Task from './Task';
+import { AiOutlineCheckCircle, AiOutlineDelete, AiOutlineEdit, AiOutlineFileDone } from 'react-icons/ai';
+import Sidebar from './Sidebar';
 
 export default function Todo() {
   const { register, setValue, getValues, watch } = useForm({
     defaultValues: {
-      value: "",
-      cari: "",
+      value: '',
+      cari: '',
     },
   });
   type todoList = {
@@ -24,13 +19,13 @@ export default function Todo() {
   const [data, addData] = useState<todoList>([]);
   const [index, setIndex] = useState<number | null>(null);
   const [btnClick, setClick] = useState(false);
+  const [filter, setFilter] = useState<todoList | null>(null);
   const [search, setSearch] = useState<todoList | null>(null);
-
-  const dataResult = search ?? data;
+  const resultData = filter ?? search ?? data;
 
   return (
     <div className="w-full p-5 flex relative flex-col-reverse md:flex-row gap-2">
-      <Sidebar />
+      <Sidebar setFilter={setFilter} data={data} />
       <div className="w-full flex justify-center min-h-screen md:min-h-[90vh] gap-2">
         <div className="w-[50rem] bg-[#60c0bf] border-2 border-black rounded-lg min-h-max p-5 px-10 mb-16 md:m-0">
           <h1 className="text-6xl font-bold">Things to do:</h1>
@@ -38,27 +33,19 @@ export default function Todo() {
             <form className="flex rounded-xl overflow-hidden border-2 border-black w-full ">
               <input
                 type="text"
-                {...register("cari")}
+                {...register('cari')}
                 className="h-full w-full outline-none py-2 px-5 bg-[#f7cb66] placeholder:font-bold text-2xl"
                 placeholder="Search a task..."
                 onChange={() => {
-                  const search = data.filter(
-                    (val) => val.value === watch("cari")
-                  );
+                  const search = data.filter((val) => val.value === watch('cari'));
                   setSearch(() => [...search]);
                 }}
               />
-              <button
-                type="submit"
-                className=" px-3 bg-black text-white text-5xl -mt-3 -scale-x-100"
-              >
+              <button type="submit" className=" px-3 bg-black text-white text-5xl -mt-3 -scale-x-100">
                 &#8981;
               </button>
             </form>
-            <button
-              className="bg-[#7fbc95] border-2 border-black rounded-xl w-2/3 text-2xl active:translate-y-0.5 duration-150"
-              onClick={() => setClick((click) => !click)}
-            >
+            <button className="bg-[#7fbc95] border-2 border-black rounded-xl w-2/3 text-2xl active:translate-y-0.5 duration-150" onClick={() => setClick((click) => !click)}>
               New task
             </button>
           </div>
@@ -66,25 +53,19 @@ export default function Todo() {
           <div
             className="w-full flex flex-col gap-3 overflow-x-auto max-h-[62.5vh] pr-1"
             style={{
-              scrollbarColor: "white #60c0bf",
-              scrollbarWidth: "thin",
+              scrollbarColor: 'white #60c0bf',
+              scrollbarWidth: 'thin',
             }}
           >
-            {dataResult.map((val, key) => (
-              <div
-                key={key}
-                className="w-full min-h-16 bg-[#b380da] border-2 flex items-center border-black rounded-xl p-3 gap-1 group/item"
-              >
+            {resultData.map((val, key) => (
+              <div key={key} className="w-full min-h-16 bg-[#b380da] border-2 flex items-center border-black rounded-xl p-3 gap-1 group/item">
                 <span className="text-lg w-full font-extrabold leading-6 relative">
                   {index === key ? (
                     <input
-                      {...register("value")}
+                      {...register('value')}
                       className="w-full px-2 py-1 rounded-md bg-[#f7cb66] outline-none h-full focus:border-black border"
                       autoFocus
-                      onKeyDown={(e) =>
-                        e.key === "Enter" &&
-                        ((val.value = getValues("value")), setIndex(null))
-                      }
+                      onKeyDown={(e) => e.key === 'Enter' && ((val.value = getValues('value')), setIndex(null))}
                     />
                   ) : (
                     val.value
@@ -92,25 +73,21 @@ export default function Todo() {
                 </span>
                 <div className="w-fit flex gap-2">
                   <button
-                    className="p-2 px-2.5 bg-[#f7cb66] rounded-xl text-2xl border border-black active:translate-y-0.5 duration-150"
-                    onClick={() =>
-                      index === null
-                        ? (setIndex(key), setValue("value", val.value))
-                        : ((val.value = getValues("value")), setIndex(null))
-                    }
+                    className="p-2 px-2.5 bg-[#f7cb66] rounded-xl text-2xl border border-black active:translate-y-0.5 active:bg-slate-500 duration-150"
+                    onClick={() => (index === null || index !== key ? (setIndex(key), setValue('value', val.value)) : (index === key && (val.value = getValues('value')), setIndex(null)))}
                   >
                     {index === key ? <AiOutlineFileDone /> : <AiOutlineEdit />}
                   </button>
                   <button
-                    className="p-2 px-2.5 bg-[#7fbc95] rounded-xl text-2xl border border-black"
-                    onClick={() => (data[key].done = !data[key].done)}
+                    className={`p-2 px-2.5 bg-[#7fbc95] rounded-xl text-2xl border border-black active:translate-y-0.5 active:bg-slate-500 ${val.done && 'bg-slate-500'}`}
+                    onClick={() => {
+                      data[key].done = !data[key].done;
+                      addData([...data]);
+                    }}
                   >
                     <AiOutlineCheckCircle />
                   </button>
-                  <button
-                    className="p-2 px-2.5 bg-[#ed7461] rounded-xl text-2xl border border-black"
-                    onClick={() => addData(data.filter((_, i) => i !== key))}
-                  >
+                  <button className="p-2 px-2.5 bg-[#ed7461] rounded-xl text-2xl border border-black active:translate-y-0.5 active:bg-slate-500" onClick={() => addData(data.filter((_, i) => i !== key))}>
                     <AiOutlineDelete />
                   </button>
                 </div>
@@ -118,7 +95,7 @@ export default function Todo() {
             ))}
           </div>
         </div>
-        <div className={`${btnClick && "w-[35rem"}`}>
+        <div className="my-auto relative">
           <Task addTodo={addData} btnEvent={btnClick} />
         </div>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,29 @@
-"use client";
+'use client';
 
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import Navbar from "@/app/component/Navbar";
-import { useState } from "react";
-import { Domain } from "@/app/component/Domain";
-import Todo from "@/app/component/Todo";
-import Task from "./component/Task";
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import Navbar from '@/app/component/Navbar';
+import { useState } from 'react';
+import { Domain } from '@/app/component/Domain';
+import Todo from '@/app/component/Todo';
+import Task from './component/Task';
 
 export default function Home() {
   const client = new ApolloClient({
-    uri: "https://ql.cloudbank.kotasatelit.com/query/",
+    uri: 'https://ql.cloudbank.kotasatelit.com/query/',
     cache: new InMemoryCache(),
   });
 
-  const [state, setState] = useState("/");
+  const [state, setState] = useState('/');
 
   return (
     <>
       <Navbar state={state} handleClick={setState} />
       <ApolloProvider client={client}>
         <div className="relative w-full overflow-hidden flex  ">
-          <div
-            className={`${
-              state === "/"
-                ? "w-full translate-x-0"
-                : "w-0 opacity-0 -translate-x-full"
-            } flex-shrink-0 duration-500 transition-all grow overflow-hidden `}
-          >
+          <div className={`${state === '/' ? 'w-full translate-x-0' : 'w-0 opacity-0 -translate-x-full'} flex-shrink-0 duration-500 transition-all grow overflow-hidden `}>
             <Todo />
           </div>
-          <div
-            className={`w-full flex-shrink-0  duration-500 transition-all ${
-              state !== "domain" ? "translate-x-full" : "translate-x-0"
-            }`}
-          >
+          <div className={`w-full flex-shrink-0  duration-500 transition-all ${state !== 'domain' ? 'translate-x-full' : 'translate-x-0'}`}>
             <Domain />
           </div>
         </div>
