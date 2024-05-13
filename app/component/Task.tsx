dbcd66a1b67cf6154678b8bab05bc3bc849f08b8
Ahmad Ramadhani <ahmadrama251005@gmail.com>
2024-05-13 21:53:23 +0700

    bug submit search

--- app/component/Task.tsx
+++ app/component/Task.tsx
@@ -1,19 +1,8 @@
-import { useForm, useFieldArray, SubmitHandler } from "react-hook-form";
-import {
-  AiOutlineClose,
-  AiTwotoneMinusCircle,
-  AiTwotonePlusCircle,
-} from "react-icons/ai";
+import { useEffect } from 'react';
+import { useForm, useFieldArray, SubmitHandler } from 'react-hook-form';
+import { AiOutlineClose, AiTwotoneMinusCircle, AiTwotonePlusCircle } from 'react-icons/ai';
 
-export default function Task({
-  addTodo,
-  btnEvent,
-  setBtn,
-}: {
-  addTodo: Function;
-  setBtn: Function;
-  btnEvent: boolean;
-}) {
+export default function Task({ addTodo, btnEvent, setBtn, setSearch }: { addTodo: Function; setBtn: Function; setSearch: any; btnEvent: boolean }) {
   type todoList = {
     tasks: {
       value: string;
@@ -27,12 +16,12 @@ export default function Task({
     formState: { errors },
   } = useForm<todoList>({
     defaultValues: {
-      tasks: [{ value: "" }],
+      tasks: [{ value: '' }],
     },
   });
   const { fields, append, remove } = useFieldArray({
     control,
-    name: "tasks",
+    name: 'tasks',
     rules: {
       minLength: 1,
       maxLength: 10,
@@ -50,11 +39,9 @@ export default function Task({
   };
 
   return (
-    <div
-      className={`font-bold bg-[#60c0bf] border-2 border-black rounded-md px-10 p-5 w-full md:w-1/2 lg:w-full m-auto relative`}
-    >
+    <div className={`font-bold bg-[#60c0bf] border-2 border-black rounded-md px-10 p-5 w-full md:w-1/2 lg:w-full m-auto relative`}>
       <h1 className="text-3xl capitalize mb-3 text-center">tambah todo</h1>
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-1">
+      <form onSubmit={handleSubmit(onSubmit, () => setSearch(null))} className="flex flex-col gap-1">
         {fields.map((field, index) => (
           <>
             <div key={field.id} className="flex gap-1">
@@ -63,13 +50,13 @@ export default function Task({
                 {...register(`tasks.${index}.value`, {
                   maxLength: {
                     value: 500,
-                    message: "panjang karakter terlalu banyak",
+                    message: 'panjang karakter terlalu banyak',
                   },
                 })}
                 style={{
-                  scrollbarColor: "white transparent",
-                  scrollbarWidth: "thin",
-                  scrollMargin: "5px",
+                  scrollbarColor: 'white transparent',
+                  scrollbarWidth: 'thin',
+                  scrollMargin: '5px',
                 }}
               />
               <button
@@ -82,35 +69,21 @@ export default function Task({
                 <AiTwotoneMinusCircle />
               </button>
             </div>
-            {errors.tasks && (
-              <span className="text-red-500 ">
-                {errors.tasks[index]?.value?.message}
-              </span>
-            )}
+            {errors.tasks && <span className="text-red-500 ">{errors.tasks[index]?.value?.message}</span>}
           </>
         ))}
         <div className="flex gap-2">
-          <button
-            type="submit"
-            className="text-xl bg-[#f7cb66] text-black border-2 border-black rounded-lg px-3 p-1"
-          >
+          <button type="submit" className="text-xl bg-[#f7cb66] text-black border-2 border-black rounded-lg px-3 p-1">
             submit
           </button>
           {fields.length > 0 && (
-            <button
-              type="button"
-              onClick={() => fields.length < 9 && append({ value: "" })}
-              className="text-xl flex justify-center items-center px-2 p-1 gap-1  "
-            >
+            <button type="button" onClick={() => fields.length < 9 && append({ value: '' })} className="text-xl flex justify-center items-center px-2 p-1 gap-1  ">
               <AiTwotonePlusCircle /> Tambah
             </button>
           )}
         </div>
       </form>
-      <button
-        className={`flex lg:hidden absolute top-0 right-0 p-3 bg-slate-700/30 rounded-bl-md z-10`}
-        onClick={() => setBtn(!btnEvent)}
-      >
+      <button className={`flex lg:hidden absolute top-0 right-0 p-3 bg-slate-700/30 rounded-bl-md z-10`} onClick={() => setBtn(!btnEvent)}>
         <AiOutlineClose />
       </button>
     </div>
